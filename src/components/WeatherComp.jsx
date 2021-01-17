9e2a419118b5aa08c1f0fcfbf127c5afe7d81c22
Dan Wang <wangdanbsb@gmail.com>
2021-01-17 16:50:21 +1100

    static display created

--- src/components/WeatherComp.jsx
+++ src/components/WeatherComp.jsx
@@ -9,17 +9,22 @@ function WeatherComp() {
                     <i className = "wi wi-day-sunny display-1"></i>
                 </h5>
                 <h1 className = "py-2">25 &deg;</h1>
+
+                {/* show max and min temperature */}
+                {minmaxTemp(24,30)}
                 
+                <h4 className="py-3">slow rain</h4>
             </div>
         </div>
     )
 }
 
-//display max and min temperature
+//function: displaying max and min temperature
 function minmaxTemp(min, max) {
     return(
         <h3>
-           
+           <span className="px-4">{min} &deg;</span>
+           <span className="px-4">{max} &deg;</span>
         </h3>
     )
 }
