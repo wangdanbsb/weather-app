af7828ce3c1ed4582cc536063c888a6e09aed974
Dan Wang <wangdanbsb@gmail.com>
2021-01-17 16:46:10 +1100

    change js file into jsx

--- src/components/WeatherComp.jsx
+++ src/components/WeatherComp.jsx
@@ -9,8 +9,18 @@ function WeatherComp() {
                     <i className = "wi wi-day-sunny display-1"></i>
                 </h5>
                 <h1 className = "py-2">25 &deg;</h1>
+                
             </div>
         </div>
+    )
+}
+
+//display max and min temperature
+function minmaxTemp(min, max) {
+    return(
+        <h3>
+           
+        </h3>
     )
 }
 
