ec6b71e1a1855b88dcc3f41f4fe5c1f3f8a94d8f
Dan Wang <wangdanbsb@gmail.com>
2021-01-17 17:13:03 +1100

    has got london's data through calling api, but not display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 
+import React from 'react'
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css" //this file is under the folder node_modules
 import "weather-icons/css/weather-icons.css"
 import WeatherComp from './components/WeatherComp';
+//api call api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
 
+const API_key = "bba35184e0fbcba66550ea1e8af36eca"
 
-function App() {
-  return (
-    <div className="App">
-      <WeatherComp></WeatherComp>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       city: undefined,
+       country: undefined
+    }
+    this.getWeather()
+  }
+  //create a method to get weather,不理解async和await用法
+  getWeather = async() => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_key}`)
+    const response = await api_call.json()
+
+    console.log(response)
+  }
+  render() {
+    return (
+      <div className="App">
+        <WeatherComp></WeatherComp>
+      </div>
+    )
+  }
 }
+
 
 export default App;
--- src/components/WeatherComp.jsx
+++ src/components/WeatherComp.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-function WeatherComp() {
+function WeatherComp(props) {
     return (
         <div className = "container">
             <div className = "cards">
-                <h1>London</h1>
+                <h1>{props.city}, {props.country}</h1>
+
                 <h5 className = "py-4"> 
                     <i className = "wi wi-day-sunny display-1"></i>
                 </h5>
+
                 <h1 className = "py-2">25 &deg;</h1>
 
                 {/* show max and min temperature */}
